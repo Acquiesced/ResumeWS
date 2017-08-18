57fa411b365609a86c66675892f3695e165bf095
Acquiesced <itsbradchow@gmail.com>
2017-08-18 00:08:39 -0400

    basis of navbar hide

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,22 @@
-document.getElementById('parallaxBanner1').ondragstart = function () { return false; };
+//basics of hiding nav bar, gotta figure it out my self further for hide on scroll down, show on scroll up.
+//gonna have to math it
 
+(function ($) {
+  $(document).ready(function(){
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 10) {
+				$('.navbar').fadeOut();
+			} else {
+				$('.navbar').fadeIn();
+			}
+		});
+
+	
+	});
+
+});
+  }(jQuery));
+
+  document.getElementById('parallaxBanner1').ondragstart = function () { return false; };
