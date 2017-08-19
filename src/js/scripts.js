3248eddad788023cea0abd5a9cee31dd7cda44ce
Acquiesced <itsbradchow@gmail.com>
2017-08-19 19:52:36 -0400

    added delayed fade based on scroll distance

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,18 +8,30 @@
 (function ($) {
     $(document).ready(function () {
         var prevScrollTop = 0;
-        const scrollDelta = 5;
+        const scrollDelta = 50; //pixels
+
         $(window).scroll(function (event) {
             var currScrollTop = $(this).scrollTop();
             var scrollDiff = currScrollTop - prevScrollTop;
-            if (currScrollTop > prevScrollTop) {
-                $('.navbar').fadeOut();
-            } else {
+
+            if (currScrollTop < prevScrollTop) {
                 $('.navbar').fadeIn();
+                prevScrollTop = currScrollTop;
+
             }
-            prevScrollTop = currScrollTop;
+            if (Math.abs(scrollDiff) > scrollDelta) {
+                console.log(scrollDiff);
+                if (currScrollTop > prevScrollTop) {
+                    $('.navbar').fadeOut();
+                }
+                else {
+                    $('.navbar').fadeIn();
+                }
+                prevScrollTop = currScrollTop;
+            }
+
         });
     });
 }(jQuery));
 
-document.getElementById('parallaxBanner1').ondragstart = function () { return false; };
+
